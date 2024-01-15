a5d6ecb6ce13bd516c8fa2188f71204ee81f7144
Jules <5582567+pondchamp@users.noreply.github.com>
2024-01-15 12:50:49 -0800

    icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import "animate.css";
 
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import React from "react";
 import { CookiesProvider } from "react-cookie";
 
@@ -9,6 +10,9 @@ const MyApp = (props: AppProps) => {
   const { Component, pageProps } = props;
   return (
     <CookiesProvider>
+      <Head>
+        <title>Julian Blair</title>
+      </Head>
       <Component {...pageProps} />
     </CookiesProvider>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&family=Rubik+Mono+One&display=swap"
           rel="stylesheet"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="favicon.ico" />
       </Head>
       <body>
         <Main />

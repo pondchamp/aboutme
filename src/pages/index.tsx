87ec709bbce55f7efbd0e2fd82f444a290f6d730
Jules <5582567+pondchamp@users.noreply.github.com>
2024-01-14 23:32:29 -0800

    css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="absolute inset-0 flex flex-col gap-2 items-center justify-center">
+    <div className="absolute inset-0 flex flex-col gap-2 items-center justify-center bg-black text-white">
       <h1 className="text-3xl">Julian J. Blair</h1>
       <div>Hello!</div>
     </div>

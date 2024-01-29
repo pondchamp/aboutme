3a9ffdc5c7dbdc5a46f8fcdbb138be99370c2571
Jules <5582567+pondchamp@users.noreply.github.com>
2024-01-29 11:17:56 -0800

    css improve

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -11,10 +11,8 @@ import { useViewport } from "@/hooks/useViewport";
 import { LayoutAnimState, SetLayoutAnimState } from "@/slice/LayoutSlice";
 
 const profileRefFinal = {
-  bottom: "82%",
-  maxWidth: "160px",
-  minWidth: "160px",
-  right: "80%",
+  height: "160px",
+  width: "120px",
 };
 
 const Title = dynamic(
@@ -23,7 +21,7 @@ const Title = dynamic(
 );
 
 export const Profile = () => {
-  const { width } = useViewport();
+  const { width, height } = useViewport();
   const { widthFlags } = useResponsiveLayout();
   const dispatch = useAppDispatch();
   const [pageViewed, setPageViewed] = usePageViewed();
@@ -34,19 +32,19 @@ export const Profile = () => {
   // Post profile mount
   useEffect(() => {
     if (
+      !profileImgRef.current ||
       !profileRef.current ||
       layoutState.layoutAnimState != LayoutAnimState.PROFILE_MOUNTED
     ) {
       return;
     }
 
+    animate(profileImgRef.current, { scale: [1, 0.7] }, { type: "keyframes" });
     animate(
       profileRef.current,
       {
-        bottom: ["0%", profileRefFinal.bottom],
-        maxWidth: [`${width}px`, profileRefFinal.maxWidth],
-        minWidth: ["0px", profileRefFinal.minWidth],
-        right: ["0%", profileRefFinal.right],
+        height: [`${height}px`, profileRefFinal.height],
+        width: [`${width}px`, profileRefFinal.width],
       },
       {
         onComplete: () => {
@@ -58,34 +56,33 @@ export const Profile = () => {
     );
   }, [
     dispatch,
+    height,
     layoutState.layoutAnimState,
-    pageViewed,
+    profileImgRef,
     profileRef,
     setPageViewed,
     width,
   ]);
 
-  useEffect(() => {
-    if (
-      !profileImgRef.current ||
-      layoutState.layoutAnimState != LayoutAnimState.PROFILE_MOUNTED
-    ) {
-      return;
-    }
-
-    animate(profileImgRef.current, { scale: [1, 0.7] }, { type: "spring" });
-  }, [dispatch, layoutState.layoutAnimState, profileImgRef]);
-
+  const { initWidth, initHeight } =
+    layoutState.layoutAnimState == LayoutAnimState.MOUNT_STARTED ||
+    layoutState.layoutAnimState == LayoutAnimState.PROFILE_MOUNTED
+      ? { initHeight: "100%", initWidth: "100%" }
+      : {
+          initHeight: profileRefFinal.height,
+          initWidth: profileRefFinal.width,
+        };
   return (
     <div
       className={`absolute top-0 left-0 flex items-center justify-center ${
         layoutState.layoutAnimState == LayoutAnimState.NOT_MOUNTED
           ? "hidden"
-          : layoutState.layoutAnimState == LayoutAnimState.MOUNT_STARTED ||
-            layoutState.layoutAnimState == LayoutAnimState.PROFILE_MOUNTED
-          ? "bottom-0 right-0"
-          : `min-w-[${profileRefFinal.minWidth}] max-w-[${profileRefFinal.maxWidth}] bottom-[${profileRefFinal.bottom}] right-[${profileRefFinal.right}]`
+          : ""
       }`}
+      style={{
+        height: initHeight,
+        width: initWidth,
+      }}
       ref={profileRef}
     >
       <div
